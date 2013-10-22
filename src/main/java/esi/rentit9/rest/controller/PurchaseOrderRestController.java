ce136e7063b6cb6816ab5dd00f25c5a8153663b0
Märt Bakhoff <mart.bakhoff@gmail.com>
2013-10-23 00:01:17 +0300

    rest: print stack on exception

--- src/main/java/esi/rentit9/rest/controller/PurchaseOrderRestController.java
+++ src/main/java/esi/rentit9/rest/controller/PurchaseOrderRestController.java
@@ -12,10 +12,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.xml.bind.annotation.XmlRootElement;
@@ -39,6 +36,12 @@ public class PurchaseOrderRestController {
 		assembler = new PurchaseOrderResourceAssembler();
         linker = new MethodLookupHelper(PurchaseOrderRestController.class);
 	}
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        ex.printStackTrace();
+        return new ResponseEntity<String>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
 	@RequestMapping("pos")
     @MethodLookup(METHOD_GET_ALL)
