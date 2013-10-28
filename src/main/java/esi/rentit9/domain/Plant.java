93088761a47f8f5ef04ff2d4a190e346c29d1a69
Märt Bakhoff <mart.bakhoff@gmail.com>
2013-10-28 14:10:55 +0200

    Plant.getPlantsBetween + test

--- src/main/java/esi/rentit9/domain/Plant.java
+++ src/main/java/esi/rentit9/domain/Plant.java
@@ -1,9 +1,10 @@
 package esi.rentit9.domain;
+
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
 import org.springframework.roo.addon.tostring.RooToString;
 
-import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 import java.util.Calendar;
 import java.util.List;
 
@@ -12,8 +13,10 @@ import java.util.List;
 @RooJpaActiveRecord
 public class Plant {
 
-    public static final String QUERY_PLANTS_BETWEEN =
-            "";
+    private static final String QUERY_PLANTS_BETWEEN =
+            "FROM Plant AS plant " +
+                    "WHERE plant.name LIKE :name AND " +
+                    "NOT EXISTS (FROM PurchaseOrderLine AS line WHERE line.plant = plant AND line.purchaseOrder.status = :postatus AND line.endDate > :start AND line.startDate < :end)";
 
     /**
      */
@@ -29,10 +32,11 @@ public class Plant {
 
 
     public static List<Plant> getPlantsBetween(String nameLike, Calendar startDate, Calendar endDate){
-        //TypedQuery<PurchaseOrderLine> query =
-        //        entityManager().createQuery(QUERY_ORDER_LINES, PurchaseOrderLine.class);
-        //query.setParameter("order", this);
-        //TODO: Query data.
-        return null;// query.getResultList();
+        TypedQuery<Plant> query = entityManager().createQuery(QUERY_PLANTS_BETWEEN, Plant.class);
+        query.setParameter("name", '%'+nameLike+'%');
+        query.setParameter("start", startDate);
+        query.setParameter("end", endDate);
+        query.setParameter("postatus", OrderStatus.Approved);
+        return query.getResultList();
     }
 }
