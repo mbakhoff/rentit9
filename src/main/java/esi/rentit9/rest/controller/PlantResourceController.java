648bb566ee8806084c8a5ecfd615ee935fb88366
Märt Bakhoff <mart.bakhoff@gmail.com>
2013-10-29 20:12:14 +0200

    rest: implemented Plants.getInTimerange

--- src/main/java/esi/rentit9/rest/controller/PlantResourceController.java
+++ src/main/java/esi/rentit9/rest/controller/PlantResourceController.java
@@ -4,6 +4,7 @@ import esi.rentit9.domain.Plant;
 import esi.rentit9.rest.PlantResource;
 import esi.rentit9.rest.PlantResourceAssembler;
 import esi.rentit9.rest.PlantResourceList;
+import org.joda.time.format.ISODateTimeFormat;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
 import java.util.Calendar;
 import java.util.List;
@@ -25,6 +27,12 @@ public class PlantResourceController {
 		assembler = new PlantResourceAssembler();
 	}
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        ex.printStackTrace();
+        return new ResponseEntity<String>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 	@RequestMapping("plants")
 	public ResponseEntity<PlantResourceList> getAllPlants() {
 		List<Plant> plants = Plant.findAllPlants();
@@ -33,14 +41,21 @@ public class PlantResourceController {
 		return response;
 	}
 
-    @RequestMapping(value="plants/find?name={name}&startDate={start}&endDate={end}", method = RequestMethod.GET)
-    public ResponseEntity<PlantResourceList> getInTimerange(@PathVariable String name, @PathVariable Calendar start, @PathVariable Calendar end) {
-        List<Plant> plants = Plant.getPlantsBetween(name, start, end);
+    @RequestMapping(value="plants/find", method = RequestMethod.GET)
+    public ResponseEntity<PlantResourceList> getInTimerange(HttpServletRequest request) {
+        List<Plant> plants = Plant.getPlantsBetween(
+                request.getParameter("name"),
+                parseDate(request.getParameter("start")),
+                parseDate(request.getParameter("end")));
         PlantResourceList resources = assembler.toResource(plants);
         return new ResponseEntity<PlantResourceList>(resources, HttpStatus.OK);
     }
 
-	@RequestMapping(value = "plants", method = RequestMethod.POST)
+    private Calendar parseDate(String dateString) {
+        return ISODateTimeFormat.yearMonthDay().parseDateTime(dateString).toGregorianCalendar();
+    }
+
+    @RequestMapping(value = "plants", method = RequestMethod.POST)
 	public ResponseEntity<Void> createPlantResource(@RequestBody PlantResource res) {
 		Plant p = new Plant();
 		p.setDescription(res.getDescription());
