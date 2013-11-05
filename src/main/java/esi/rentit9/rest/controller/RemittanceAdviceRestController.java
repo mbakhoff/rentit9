c348fc890b2914a7e18ec2170d459f0ecb8f38bd
Märt Bakhoff <mart.bakhoff@gmail.com>
2013-11-06 00:15:38 +0200

    renamed remittance controller

--- src/main/java/esi/rentit9/rest/controller/RemittanceAdviceRestController.java
+++ src/main/java/esi/rentit9/rest/controller/RemittanceAdviceRestController.java
@@ -1,7 +1,8 @@
 package esi.rentit9.rest.controller;
 
-import java.net.URI;
-
+import esi.rentit9.domain.RemittanceAdvice;
+import esi.rentit9.rest.RemittanaceAdviceResource;
+import esi.rentit9.rest.util.MethodLookup;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,18 +12,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import esi.rentit9.domain.RemittanceAdvice;
-import esi.rentit9.rest.RemittanaceAdviceResource;
-import esi.rentit9.rest.util.MethodLookup;
+import java.net.URI;
 
 @Controller
 @RequestMapping("/rest/")
-public class RemittanceAdviceController {
+public class RemittanceAdviceRestController {
     public static final int METHOD_CREATE_RA = 1;
 
     @RequestMapping(value = "ra", method = RequestMethod.POST)
     @MethodLookup(METHOD_CREATE_RA)
-	public ResponseEntity<Void> createOrder(@RequestBody RemittanaceAdviceResource res) {
+	public ResponseEntity<Void> createRemittance(@RequestBody RemittanaceAdviceResource res) {
 		RemittanceAdvice remittanceAdvice = new RemittanceAdvice();
 		remittanceAdvice.setInvoice(res.getInvoice());
 		remittanceAdvice.setPayDay(res.getPayDay());
@@ -33,7 +32,7 @@ public class RemittanceAdviceController {
 				ServletUriComponentsBuilder.fromCurrentRequestUri().
 						pathSegment(remittanceAdvice.getId().toString()).build().toUri();
 		headers.setLocation(location);
-		headers.add("RentItId", (remittanceAdvice.getId()).toString());
+		headers.add("RentItId", remittanceAdvice.getId().toString());
 		return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
 	}
 }
