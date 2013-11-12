f835f3bbc2b296a1dcccbccb30a8661e52480ea5
Märt Bakhoff <mart.bakhoff@gmail.com>
2013-11-13 01:37:39 +0200

    added auth to interop

--- src/main/java/esi/rentit9/interop/Team9Rest.java
+++ src/main/java/esi/rentit9/interop/Team9Rest.java
@@ -2,26 +2,20 @@ package esi.rentit9.interop;
 
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
 import esi.rentit9.domain.PurchaseOrder;
-import esi.rentit9.soap.PurchaseOrderAssembler;
 
 public class Team9Rest implements BuilditInterop.Rest {
 
     public static final String BUILDIT_POS = "https://buildit9.herokuapp.com/rest/pos";
 
-    private final PurchaseOrderAssembler assembler;
-
-    public Team9Rest() {
-        assembler = new PurchaseOrderAssembler();
-    }
-
     @Override
     public void sendAccept(PurchaseOrder order) {
-        ClientResponse createRequest = Client.create()
+        ClientResponse response = getClient()
                 .resource(getConfirmUrl(order))
                 .post(ClientResponse.class);
 
-        int status = createRequest.getStatus();
+        int status = response.getStatus();
         if (status != ClientResponse.Status.OK.getStatusCode()) {
             throw new InteropException();
         }
@@ -29,14 +23,20 @@ public class Team9Rest implements BuilditInterop.Rest {
 
     @Override
     public void sendReject(PurchaseOrder order) {
-        ClientResponse createRequest = Client.create()
+        ClientResponse response = getClient()
                 .resource(getConfirmUrl(order))
                 .delete(ClientResponse.class);
 
-        int status = createRequest.getStatus();
+        int status = response.getStatus();
         if (status != ClientResponse.Status.OK.getStatusCode()) {
             throw new InteropException();
         }
+    }
+
+    private static Client getClient() {
+        Client client = Client.create();
+        client.addFilter(new HTTPBasicAuthFilter("user", "user"));
+        return client;
     }
 
     private static String getConfirmUrl(PurchaseOrder order) {
