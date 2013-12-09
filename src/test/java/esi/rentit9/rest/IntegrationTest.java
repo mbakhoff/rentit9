a854491f14a146c725fa66aa9a2a029853446c79
Märt Bakhoff <mart.bakhoff@gmail.com>
2013-12-09 16:32:42 +0200

    updated rentit gmail password

--- src/test/java/esi/rentit9/rest/IntegrationTest.java
+++ src/test/java/esi/rentit9/rest/IntegrationTest.java
@@ -1,28 +1,11 @@
 package esi.rentit9.rest;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import javax.mail.Message;
-import javax.mail.internet.MimeMessage;
 
 @ContextConfiguration(locations = "classpath:/META-INF/spring/applicationContext*.xml")
 @RunWith(SpringJUnit4ClassRunner.class)
 public class IntegrationTest {
 
-    @Autowired
-    private JavaMailSender smtp;
-
-    @Test
-    public void testSmtpConfig() throws Exception {
-        MimeMessage msg = smtp.createMimeMessage();
-        msg.setSubject("testSmtpConfig");
-        msg.setText("test runner at rentit9");
-        msg.setRecipients(Message.RecipientType.TO, "rentit9esi@gmail.com");
-        smtp.send(msg);
-    }
 }
